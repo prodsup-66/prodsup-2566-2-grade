e4026baaffc157c3820ec8aa018c16a6a503f1e0
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-03-09 22:00:58 +0700

    Update display

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
-import { data } from "./db";
+import { getData } from "./db";
 import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 function App() {
   const [studentId, setStudentId] = useState("");
   const [email, setEmail] = useState("");
   const [courseNumber, setCourseNumber] = useState("");
-  const [grade, setGrade] = useState<null | (typeof data)[0]>(null);
+  const [grade, setGrade] = useState<null | ReturnType<typeof getData>[0]>(
+    null
+  );
   const [parent] = useAutoAnimate(/* optional config */);
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
@@ -22,6 +24,7 @@ function App() {
   }
 
   function handleSubmit() {
+    const data = getData();
     const student = data.find((s) => s.student_id === studentId);
     if (!student) {
       alert("Student ID is not correct");
@@ -47,44 +50,63 @@ function App() {
   }
 
   return (
-    <div className="container-fluid">
-      <h1>Production Support 2566-2 Grade</h1>
-      <article>
-        <label htmlFor="student-id">Student ID</label>
-        <input
-          type="number"
-          name="student-id"
-          onChange={handleChange}
-          value={studentId}
-        />
-        <label htmlFor="email">CMU Email</label>
-        <input
-          type="email"
-          name="email"
-          onChange={handleChange}
-          value={email}
-        />
-        <label htmlFor="course-number">Course Number (ุ6 digit)</label>
-        <input
-          type="number"
-          name="course-number"
-          onChange={handleChange}
-          value={courseNumber}
-        />
-        <div role="group">
-          <button onClick={handleSubmit}>Submit</button>
-          <button onClick={handleReset}>Reset</button>
-        </div>
-      </article>
+    <>
+      <header className="container">
+        <h1>Production Support 2566-2 Grade</h1>
+      </header>
+      <main className="container">
+        <article>
+          <label htmlFor="student-id">Student ID</label>
+          <input
+            type="number"
+            name="student-id"
+            onChange={handleChange}
+            value={studentId}
+          />
+          <label htmlFor="email">CMU Email</label>
+          <input
+            type="email"
+            name="email"
+            onChange={handleChange}
+            value={email}
+          />
+          <label htmlFor="course-number">Course Number (ุ6 digit)</label>
+          <input
+            type="number"
+            name="course-number"
+            onChange={handleChange}
+            value={courseNumber}
+          />
+          <div role="group">
+            <button onClick={handleSubmit}>Submit</button>
+            <button onClick={handleReset}>Reset</button>
+          </div>
+        </article>
 
-      <div ref={parent}>
-        {grade && (
-          <article>
-            <pre>{JSON.stringify(grade, null, 2)}</pre>
-          </article>
-        )}
-      </div>
-    </div>
+        <div ref={parent}>
+          {grade && (
+            <>
+              <article>
+                <pre>{JSON.stringify(grade, null, 2)}</pre>
+              </article>
+              <article>
+                <h4>คำอธิบาย</h4>
+                <ul>
+                  <li>
+                    คำนำหน้า NR = อ.นิรันดร์, AC = อ.อนิรุท, SR = อ.ศักดิ์เกษม
+                  </li>
+                  <li>
+                    เลขในวงเล็บคือคะแนนดิบเต็ม อย่างเช่น (5)
+                    คือรายการที่มีคะแนนดิบเต็ม 5
+                  </li>
+                  <li>เลขในวงเล็บที่มี % คือคะแนนที่สเกลให้เป็นคะแนนตัดเกรด</li>
+                </ul>
+              </article>
+            </>
+          )}
+        </div>
+      </main>
+    </>
   );
 }
 
